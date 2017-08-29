5b75421e620fcfafb22c5fce85486013d73a5853
橄榄 <ganlan@yysp.me>
2017-08-29 17:44:24 +0800

    UIMainLogin.scene 这个初始化应该放到  UIMainLogin 下面 bug 就解决了

--- src/login/main_login.js
+++ src/login/main_login.js
@@ -1,47 +1,40 @@
+/**
+ * penny
+ */
 
-//////////////////////////////////////////////////////////////////////////
-//账号login
-//////////////////////////////////////////////////////////////////////////
 var UIMainLogin = cc.Node.extend({
     _am: null,
     ctor: function () {
         this._super();
-        var szWinSize = cc.director.getWinSize();
+        var winSize = cc.director.getWinSize();
         var delayTime = 0.5;
         if (cc.sys.os !== cc.sys.OS_IOS) {
             delayTime = 1.0;
         }
 
-        var bgLogin = cc.Sprite.create("res/login/login.jpg");
-        this.addChild(bgLogin, -1);
-        bgLogin.attr({
-            x: szWinSize.width / 2,
-            y: szWinSize.height / 2
+        var login_bg = new cc.Sprite("res/login/login.jpg");
+        this.addChild(login_bg, -1);
+        login_bg.attr({
+            x: winSize.width / 2,
+            y: winSize.height / 2
         });
 
         var self = this;
         this.scheduleOnce(function (dt) {
-            if (!cc.sys.isNative || cc.sys.OS_WINDOWS === cc.sys.os) { // web
+            if (!cc.sys.isNative || cc.sys.OS_WINDOWS === cc.sys.os) {
+                // self._toEnter();
+            } else {
+                // self._toLoad();
                 // self._toEnter();
             }
-            else { // mobile
-                // self._toLoad();
-            }
         }, delayTime);
+
     }
 });
 
 UIMainLogin.scene = function () {
-    var ss = new cc.Scene();
+    var scene = new cc.Scene();
     var mainLogin = new UIMainLogin();
-    ss.addChild(mainLogin);
-    return ss;
+    scene.addChild(mainLogin);
+    return scene;
 };
-
-
-
-
-
-
-
-
